3ed79ae53836d76097f25a154bd664463a47760a
Oybek <oybekgaibov949@gmail.com>
2024-11-08 07:49:56 +0500

    harakatini darobotkasi qoldi

--- ball/balls.py
+++ ball/balls.py
@@ -2,54 +2,51 @@ import pygame
 import random
 pygame.init()
 screen = pygame.display.set_mode((800,600))
-all_sprites = pygame.sprite.Group()
-class Ball(pygame.sprite.Sprite):
-	def __init__(self,x,y):
-
-		super().__init__()
-		self.image = pygame.Surface((20,20))
-
-		self.image.fill((255,0,0))
-		self.rect = self.image.get_rect()
-		self.rect.x = x
-		self.rect.y = y
-		
-
-
-
-for i in range(10):
-	ball = Ball(i*30,50)
-	all_sprites.add(ball)
 
 
 clock = pygame.time.Clock()
-clock.tick(60)
-speed =random.randint(-5,5)
+clock.tick(15)
+
+class Ball():
+	def __init__(self,coord= None,color= None,rad=30):
+
+		if coord == None:
+			coord =(random.randint(0,800-rad),random.randint(0,600-rad))
+		self.coord =list(coord)
+		if color == None:
+			color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+		
+		self.color = color
+		
+		self.rad = rad
+		self.image = pygame.draw.circle(screen,self.color,self.coord,self.rad)
+		
+		self.vx =	random.randint(-10,10)
+		self.vy = random.randint(-10,10)
+
+	def update(self):
+		self.coord[0] += self.vx
+		self.coord[1] += self.vy
+
+		if self.coord[0]<0 or self.coord[0] + self.rad*2>800:
+			self.vx = -self.vx
+		if self.coord[1] < 0 or self.coord[1] + self.rad*2 >600:
+			self.vy = -self.vy
+
+balls=[]
+for i in range(10):
+	ball = Ball()
+	balls.append(ball)
+	
+	
 
 runing = True
 while runing:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			runing = False
-
-	for sprite in all_sprites:
-		sprite.rect.y += speed
-		sprite.rect.x +=speed
-	if sprite.rect.x>=800:
-		sprite.rect.x = 700
-	elif sprite.rect.x <=0:
-		sprite.rect.x = 100
-	elif sprite.rect.y <=0:
-		sprite.rect.y = 100
-	elif sprite.rect.y >=600:
-		sprite.rect.y =500
-
-	screen.fill((255,255,255))
-
-	all_sprites.update()
-	
-	
-	all_sprites.draw(screen)
-
+	for ball in balls:
+        ball.update()
+        screen.blit(ball.image, ball.coord)
 	pygame.display.flip()
 pygame.QUIT
