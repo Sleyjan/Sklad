f1ccb9fa1abab3da19cd34594b1e0bbf176a1fab
Oybek <oybekgaibov949@gmail.com>
2024-11-10 23:09:03 +0500

    Ohiriga yetkazildi randomna tuwvoti sharlar tepadan pastka

--- ball/balls.py
+++ ball/balls.py
@@ -5,43 +5,71 @@ screen = pygame.display.set_mode((800,600))
 
 
 clock = pygame.time.Clock()
-clock.tick(15)
+fps = 60
 
 class Ball():
-	def __init__(self,coord= None,color= None,rad=30):
+	def __init__(self,x,y,color= None,rad=30):
 
-		if coord == None:
-			coord =(random.randint(0,800-rad),random.randint(0,600-rad))
-		self.coord =list(coord)
+		self.rad = rad
+
 		if color == None:
 			color = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
+
 		self.color = color
-		self.rad = rad	
-		self.vx =	random.randint(-10,10)
-		self.vy = random.randint(-10,10)
+
+		self.image = pygame.Surface((self.rad * 2, self.rad * 2), pygame.SRCALPHA)
+
+		pygame.draw.circle(self.image, self.color, (self.rad, self.rad), self.rad)
+		
+
+		self.vx = random.randint(-5, 5)
+
+		self.vy = random.randint(1, 2)
+
+		self.x = x if x and y else random.randint(self.rad, 800 - self.rad)
+
+		self.y = y if x and y else random.randint(self.rad, 600 - self.rad)
 
 	def update(self):
-        self.image = pygame.draw.circle(screen, self.color, self.coord, self.rad)
-        self.coord[0] += self.vx
-        self.coord[1] += self.vy
+        
+		 friction = 1  # Adjust for desired friction level (0 to 1)
+		 self.vx *= friction 
+		 self.vy *= friction
+		 #self.x += self.vx
+		 self.y += self.vy
 
-		if self.coord[0]<0 or self.coord[0] + self.rad*2>800:
-			self.vx = -self.vx
-		if self.coord[1] < 0 or self.coord[1] + self.rad*2 >600:
-			self.vy = -self.vy
-balls=[]
-for i in range(10):
-	ball = Ball()
-	balls.append(ball)
+        
+		 #if self.x < 0:
+		 #	self.x = 0
+		 	#self.vx = -self.vx
+		 #elif self.x + self.rad * 2 > 800:
+		 	#self.x = 800 - self.rad * 2
+		 #	self.vx = -self.vx
 
+		 if self.y == 0:
+		 	self.y = 0
+		 	self.vy = -self.vy
+		 elif self.y + self.rad * 2 > 600:
+		 	self.x = random.randint(self.rad, 800 - self.rad)
+		 	self.y = 600 - self.rad * 2
+		 	self.y = 0
+		 	self.vy = +self.vy
 
+balls = [Ball(None,None) for i in range(6)]
 running = True
 while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
-      runing = False
-  # Correct indentation (assuming 2 spaces)
+      running = False
+ 	
+  screen.fill((0,0,0))
+
   for ball in balls:
     ball.update()
-    screen.blit(ball.image, ball.coord)
+    screen.blit(ball.image,(ball.x,ball.y))
+
   pygame.display.flip()
+
+  clock.tick(fps)
+
+pygame.quit()
