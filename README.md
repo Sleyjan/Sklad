ba0b702abe52a08890185f85295a45432ef073f1
Sleyjan <oybekgaibov949@gmail.com>
2024-12-07 22:53:26 +0500

    Добавил вес список склда

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Sklad
+This programm for using in life many time excel docs. 
+With this programm you can find  faster your result faster by keyword which you remember.
+ANd you can open and write, change results.
+Thanks
--- searcher.py
+++ searcher.py
@@ -1,24 +1,34 @@
 import openpyxl
 
+searching = True
+
 def find_keyword(filename, keyword):
-  """Ищет указанное ключевое слово во всех ячейках Excel файла.
+    global searching
+    try:
+        workbook = openpyxl.load_workbook(filename)
+        sheet = workbook.active
 
-  Args:
-    filename: Имя файла Excel.
-    keyword: Искомое ключевое слово.
-  """
+        if not keyword:
+            searching = False
 
-  workbook = openpyxl.load_workbook(filename)
-  sheet = workbook.active  # Выбираем активный лист
-  print(keyword)
+        if keyword:
+            for row_idx, row in enumerate(sheet.iter_rows()):
+                for cell in row:
+                    if keyword.lower() in str(cell.value).lower():
+                        straka = [str(cell.value) for cell in row]
+                        print(file, '--'.join(straka[1:5]))
 
-  for row_idx, row in enumerate(sheet.iter_rows()):
-    for cell in row:
-      if keyword in str(cell.value):
-        straka = [str(cell.value) for cell in row]
-        print('--'.join(straka[1:5]))
+    except FileNotFoundError:
+        print(f"Файл {filename} не найден.")
+    except Exception as e:
+        print(f"Произошла ошибка: {str(e)}")
+
+excelist =['Ткацкая Lohia.xlsx','Швейка New Long.xlsx','Экструдер Lohia.xlsx','6 m Сетка.xlsx','Bayby Lofil 20HT-80HT.xlsx','Gabbar швейка.xlsx','Дробилка Hao You.xlsx','Камерсиогка Lohia.xlsx','Камерсионка Hoa You.xlsx','Компрессор..xlsx','Крутилка.xlsx','Ламинатор Hoa You.xlsx','Ламинатор Hoa You.xlsx','Общые веши.xlsx','Печать.xlsx','Подшипники.xlsx','Ролики Ткацкий.xlsx','Склад 1чи Хона 2 кават.xlsx','Страпа.xlsx','Страпа Хитой.xlsx','Швейка китай склад.xlsx','Шпула резка.xlsx']
+
+excel_dir ='/home/oybek/excel/'
 
 
+for file in excelist:
+    find_keyword(excel_dir + file, '')
 
-# Пример использования
-find_keyword('/home/oybek/Ткацкая Lohia.xlsx', 'rh')
+
