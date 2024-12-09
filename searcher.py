33e7c27736012c231b96ff503ab5f58dcd9d9658
Sleyjan <oybekgaibov949@gmail.com>
2024-12-09 16:50:48 +0500

    работаю под интерфейсом спосиба Гемини почти много что она сама помогла. Библиатека PyQT5

--- searcher.py
+++ searcher.py
@@ -1,7 +1,62 @@
-import openpyxl
+import sys
+import os
+from importlib.resources import files
+from platform import system
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QFileDialog
+from docutils.nodes import option
+from PyQt5.QtCore import QThread,pyqtSignal
 
-searching = True
+#import openpyxl
+class WorkerThread(QThread):
+    finished = pyqtSignal()
+    result = pyqtSignal(list)
 
+    def __init__(self, files):
+        super().__init__()
+        self.files = files
+
+    def run(self):
+        processed_files = []
+        processed_files.append(files)
+        print(self.files)
+
+        self.result.emit(processed_files)  # Передать обработанные файлы
+        self.finished.emit()
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+    def initUI(self):
+        self.btn = QPushButton('open',self)
+        self.btn.move(20,20)
+        self.btn.clicked.connect(self.showDialog)
+        self.setGeometry(300,300,250,150)
+        self.setWindowTitle('allFiles')
+        self.show()
+    def showDialog(self):
+        options = QFileDialog.Option()
+        options |= QFileDialog.DontUseNativeDialog
+        files, _=QFileDialog.getOpenFileNames(self,"QFileDialog.getOpenFileNames()","","ALl Files(*);;Excel Files(*.xlsx)")
+        if files:
+            self.worker_thread = WorkerThread(files)
+            self.worker_thread.finished.connect(self.on_finished)
+            self.worker_thread.result.connect(self.on_result)
+            self.worker_thread.start()
+
+    def on_finished(self):
+        print("Обработка файлов завершена")  # Пример действий по завершении
+        self.worker_thread = None  # Очищаем ссылку на worker_thread
+
+    def on_result(self, processed_files):
+        print(processed_files)
+if __name__ =="__main__":
+     app = QApplication(sys .argv)
+     ex =MyApp()
+     sys.exit(app.exec_())
+
+#searching = True
+"""
 def find_keyword(filename, keyword):
     global searching
     try:
@@ -30,5 +85,5 @@ excel_dir ='/home/oybek/excel/'
 
 for file in excelist:
     find_keyword(excel_dir + file, '')
-
+"""
 
